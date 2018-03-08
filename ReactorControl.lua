073a93c23e744fa1c344de60d63328d1b11af293
Chris Schultz <cschultz6@gmail.com>
2018-03-07 20:27:40 -0500

    V0.3 imported from dropbox

--- ReactorControl.lua
+++ ReactorControl.lua
@@ -3,10 +3,6 @@
 --Core Display Functions
 --MonitorData (constantly call DisplayData)
 
---Automated Control Monitoring and Response Test
---checkForReactorHeatResponse	(Do we need to do something because of the current heat level)
---checkForBatteryPowerResponse	(Do we need to do something because of the current power level)
-
 --Control Functions
 --StartAutomation
 
@@ -123,10 +119,7 @@ local function getReactorHeatPercentage()
 end
 
 local function setMaxReactorHeatPercentage()
-	local labelsize=36
-	local valuesize=12
-	local maxtextsize=48
-	padding = ((width - maxtextsize * 2)) / 3
+	local input
 	repeat
 	gpu.setForeground(0x00FFFF)
 	io.write("Current Maximum Heat Percentage:")
@@ -135,35 +128,39 @@ local function setMaxReactorHeatPercentage()
 	print()
 	gpu.setForeground(0xFFFF00)
 	io.write("Input New Maximum Heat Percentage:")
-	local input = tonumber(io.read())
+	input = tonumber(io.read())
 	until not (input == nil or input > 100 or input < 0)
 	MaxReactorHeatPercentage = input
 end
 
 local function setMaxBatteryPowerPercentage()
-	local labelsize=36
-	local valuesize=12
-	local maxtextsize=48
-	padding = ((width - maxtextsize * 2)) / 3
+	local input
 	repeat
 	gpu.setForeground(0x00FFFF)
 	io.write("Current Maximum Power Percentage:")
 	gpu.setForeground(0xFFFFFF)
 	io.write(tostring(MaxBatteryPowerPercentage))
 	print()
+	gpu.setForeground(0x00FFFF)
+	io.write("Current Minimum Power Percentage:")
+	gpu.setForeground(0xFFFFFF)
+	io.write(tostring(MinBatteryPowerPercentage))
+	print()
 	gpu.setForeground(0xFFFF00)
 	io.write("Input New Maximum Power Percentage:")
-	local input = tonumber(io.read())
-	until not (input == nil or input > 100 or input < 0)
+	input = tonumber(io.read())
+	until not (input == nil or input > 100 or input < MinBatteryPowerPercentage)
 	MaxBatteryPowerPercentage = input
 end
 
 local function setMinBatteryPowerPercentage()
-	local labelsize=36
-	local valuesize=12
-	local maxtextsize=48
-	padding = ((width - maxtextsize * 2)) / 3
+	local input
 	repeat
+	gpu.setForeground(0x00FFFF)
+	io.write("Current Maximum Power Percentage:")
+	gpu.setForeground(0xFFFFFF)
+	io.write(tostring(MaxBatteryPowerPercentage))
+	print()
 	gpu.setForeground(0x00FFFF)
 	io.write("Current Minimum Power Percentage:")
 	gpu.setForeground(0xFFFFFF)
@@ -171,9 +168,35 @@ local function setMinBatteryPowerPercentage()
 	print()
 	gpu.setForeground(0xFFFF00)
 	io.write("Input New Minimum Power Percentage:")
-	local input = tonumber(io.read())
-	until not (input == nil or input > 100 or input < 0)
+	input = tonumber(io.read())
+	until not (input == nil or input > MaxBatteryPowerPercentage or input < 0)
 	MinBatteryPowerPercentage = input
+end
+
+--For automated control and test, do we need to do something because of the reactor heat?
+--if so, run appropriate action and return true, else return false
+--calling function must handle UI explanation of result
+local function checkForReactorHeatResponse(CurrentHeatPercentage)
+	if CurrentHeatPercentage >= MaxReactorHeatPercentage then
+		SCRAMset()
+		Alarmset()
+		return true
+	end
+	return false
+end
+
+--For automated control and test, do we need to do something because of the battery power?
+--if so, run appropriate action
+--calling function must handle UI explanation of result
+local function checkForBatteryPowerResponse(CurrentPowerPercentage)
+	if CurrentPowerPercentage >= MaxBatteryPowerPercentage then
+		StopReactor()
+		return true
+	elseif CurrentPowerPercentage <= MinBatteryPowerPercentage then
+		StartReactor()
+		return true
+	end
+	return false
 end
 
 local function PrintTestResult(TestText, ResultText, Pass)
@@ -281,6 +304,7 @@ local function SCRAMtest()
 	print(result)
 	gpu.setForeground(0xFFFFFF)
 	print()
+	return result == "Passed"
 end
 
 local function Alarmtest()
@@ -370,6 +394,293 @@ local function Alarmtest()
 	print(result)
 	gpu.setForeground(0xFFFFFF)
 	print()
+	return result == "Passed"
+end
+
+local function HeatHightest()
+	local result = "Passed"
+	local response
+	
+	print("Initiating High Heat Response Test")
+	local testtext = "Initial SCRAM Status............................. "
+	if getSCRAMStatus() then
+		PrintTestResult(testtext, "Triggered", false)
+		gpu.setForeground(0xFF00FF)
+		print("Test aborted. Ensure safe plant status and clear SCRAM.")
+		print()
+		return
+	else
+		PrintTestResult(testtext, "Ready", true)
+	end
+	
+	SCRAMreset()
+	Alarmreset()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing 0 Heat Response")
+	response = checkForReactorHeatResponse(0)
+	testtext = "Checking 0 Heat Response result.................. "
+	if response then
+		PrintTestResult(testtext, "Triggered", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "No Response", true)
+	end
+	os.sleep(1)
+	testtext = "Checking SCRAM status after heat response test... "
+	if getSCRAMStatus() then
+		PrintTestResult(testtext, "Triggered", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "Ready", true)
+	end
+
+	SCRAMreset()
+	Alarmreset()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing Border Heat Response")
+	response = checkForReactorHeatResponse(MaxReactorHeatPercentage-0.01)
+	testtext = "Checking Border Heat Response result............. "
+	if response then
+		PrintTestResult(testtext, "Triggered", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "No Response", true)
+	end
+	os.sleep(1)
+	testtext = "Checking SCRAM status after heat response test... "
+	if getSCRAMStatus() then
+		PrintTestResult(testtext, "Triggered", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "Ready", true)
+	end
+
+	SCRAMreset()
+	Alarmreset()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing Limit Heat Response")
+	response = checkForReactorHeatResponse(MaxReactorHeatPercentage)
+	testtext = "Checking Limit Heat Response result.............. "
+	if response then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "No Response", false)
+		result = "Failed"
+	end
+	os.sleep(1)
+	testtext = "Checking SCRAM status after heat response test... "
+	if getSCRAMStatus() then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "Ready", false)
+		result = "Failed"
+	end
+
+	gpu.setForeground(0xFFFFFF)
+	SCRAMreset()
+	Alarmreset()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing 100% Heat Response")
+	response = checkForReactorHeatResponse(100)
+	testtext = "Checking 100% Heat Response result............... "
+	if response then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "No Response", false)
+		result = "Failed"
+	end
+	os.sleep(1)
+	testtext = "Checking SCRAM status after heat response test... "
+	if getSCRAMStatus() then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "Ready", false)
+		result = "Failed"
+	end
+
+	SCRAMreset()
+	Alarmreset()
+	gpu.setForeground(0x00FFFF)
+	io.write("High Heat Response Test Complete: ")
+	if result == "Passed" then
+		gpu.setForeground(0x00FF00)
+	else
+		gpu.setForeground(0xFF0000)
+	end
+	print(result)
+	gpu.setForeground(0xFFFFFF)
+	print()
+	return result == "Passed"
+end
+
+local function Powertest()
+	local initialreactorstatus = getReactorStatus()
+	local result = "Passed"
+	local response
+	
+	print("Initiating High/Low Power Response Test")
+	local testtext = "Initial SCRAM Status............................. "
+	if getSCRAMStatus() then
+		PrintTestResult(testtext, "Triggered", false)
+		gpu.setForeground(0xFF00FF)
+		print("Test aborted. Ensure safe plant status and clear SCRAM.")
+		print()
+		return
+	else
+		PrintTestResult(testtext, "Ready", true)
+	end
+	
+	if not initialreactorstatus then
+		--TEST REACTOR POWER UP
+		testtext = "Reactor Startup.................................. "
+		StartReactor()
+		os.sleep(1)
+		if getReactorStatus() then
+			PrintTestResult(testtext, "Started", true)
+		else
+			PrintTestResult(testtext, "Stopped", false)
+			gpu.setForeground(0xFF00FF)
+			print("Unable to power reactor.")
+			result = "Failed"
+		end
+	end
+
+	gpu.setForeground(0xFFFFFF)
+	print("Testing 0 Power Response")
+	response = checkForBatteryPowerResponse(0)
+	testtext = "Checking 0 Power Response result................. "
+	if response then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "No Response", false)
+		result = "Failed"
+	end
+	os.sleep(1)
+	testtext = "Checking start status after power response test.. "
+	if getReactorStatus() then
+		PrintTestResult(testtext, "Started", true)
+	else
+		PrintTestResult(testtext, "Stopped", false)
+		result = "Failed"
+	end
+
+	StopReactor()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing Low Limit Heat Response")
+	response = checkForBatteryPowerResponse(MinBatteryPowerPercentage)
+	testtext = "Checking Low Limit Power Response result......... "
+	if response then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "No Response", false)
+		result = "Failed"
+	end
+	os.sleep(1)
+	testtext = "Checking start status after power response test.. "
+	if getReactorStatus() then
+		PrintTestResult(testtext, "Started", true)
+	else
+		PrintTestResult(testtext, "Stopped", false)
+		result = "Failed"
+	end
+
+	StopReactor()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing Low Border Power Response")
+	response = checkForBatteryPowerResponse(MinBatteryPowerPercentage+0.01)
+	testtext = "Checking Low Border Power Response result........ "
+	if response then
+		PrintTestResult(testtext, "Triggered", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "No Response", true)
+	end
+	os.sleep(1)
+	testtext = "Checking start status after power response test.. "
+	if getReactorStatus() then
+		PrintTestResult(testtext, "Started", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "Stopped", true)
+	end
+
+	StartReactor()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing High Border Power Response")
+	response = checkForBatteryPowerResponse(MaxBatteryPowerPercentage-0.01)
+	testtext = "Checking High Border Power Response result....... "
+	if response then
+		PrintTestResult(testtext, "Triggered", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "No Response", true)
+	end
+	os.sleep(1)
+	testtext = "Checking start status after power response test.. "
+	if getReactorStatus() then
+		PrintTestResult(testtext, "Started", true)
+	else
+		PrintTestResult(testtext, "Stopped", false)
+		result = "Failed"
+	end
+
+	StartReactor()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing High Limit Heat Response")
+	response = checkForBatteryPowerResponse(MaxBatteryPowerPercentage)
+	testtext = "Checking High Limit Power Response result........ "
+	if response then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "No Response", false)
+		result = "Failed"
+	end
+	os.sleep(1)
+	testtext = "Checking start status after power response test.. "
+	if getReactorStatus() then
+		PrintTestResult(testtext, "Started", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "Stopped", true)
+	end
+
+	StartReactor()
+	gpu.setForeground(0xFFFFFF)
+	print("Testing 100% Power Response")
+	response = checkForBatteryPowerResponse(100)
+	testtext = "Checking 100% Power Response result.............. "
+	if response then
+		PrintTestResult(testtext, "Triggered", true)
+	else
+		PrintTestResult(testtext, "No Response", false)
+		result = "Failed"
+	end
+	os.sleep(1)
+	testtext = "Checking start status after power response test.. "
+	if getReactorStatus() then
+		PrintTestResult(testtext, "Started", false)
+		result = "Failed"
+	else
+		PrintTestResult(testtext, "Stopped", true)
+	end
+
+	if not initialreactorstatus then
+		StopReactor()
+	else
+		StartReactor()
+	end
+
+	gpu.setForeground(0x00FFFF)
+	io.write("High/Low Power Response Test Complete: ")
+	if result == "Passed" then
+		gpu.setForeground(0x00FF00)
+	else
+		gpu.setForeground(0xFF0000)
+	end
+	print(result)
+	gpu.setForeground(0xFFFFFF)
+	print()
+	return result == "Passed"
 end
 
 local function DisplayMainMenu()
@@ -387,26 +698,24 @@ local function DisplayMainMenu()
 	--write first line (3 entries)
 	io.write(text.padLeft("", padding))
 	WriteMenuItem(1, "Automated Control", maxtextsize+padding)
-	WriteMenuItem(5, "Start Reactor", maxtextsize+padding)
-	WriteMenuItem(9, "Full Control System Diagnostic", maxtextsize)
+	WriteMenuItem(4, "Print Plant Status", maxtextsize+padding)
+	WriteMenuItem(7, "Emergency SCRAM", maxtextsize)
 	print()
 	
 	io.write(text.padLeft("", padding))
-	WriteMenuItem(2, "Set Automated Control Parameters", maxtextsize+padding)
-	WriteMenuItem(6, "Stop Reactor", maxtextsize+padding)
-	WriteMenuItem(10, "Manual Tests", maxtextsize)
+	WriteMenuItem(2, "Start Reactor", maxtextsize+padding)
+	WriteMenuItem(5, "Set Automated Control Parameters", maxtextsize+padding)
+	WriteMenuItem(8, "Reset Emergency SCRAM", maxtextsize)
 	print()
 	
 	io.write(text.padLeft("", padding))
-	WriteMenuItem(3, "Print Plant Status", maxtextsize+padding)
-	WriteMenuItem(7, "Emergency SCRAM", maxtextsize+padding)
-	WriteMenuItem(11, "Reset Alarm", maxtextsize)
+	WriteMenuItem(3, "Stop Reactor", maxtextsize+padding)
+	WriteMenuItem(6, "Manual Tests", maxtextsize+padding)
+	WriteMenuItem(9, "Reset Alarm", maxtextsize)
 	print()
 	
-	io.write(text.padLeft("", padding))
-	WriteMenuItem(4, "Monitor Plant Status", maxtextsize+padding)
-	WriteMenuItem(8, "Reset Emergency SCRAM", maxtextsize+padding)
-	WriteMenuItem(12, "Exit Program", maxtextsize)
+	io.write(text.padLeft("", padding+maxtextsize+padding+maxtextsize+padding+2))
+	WriteMenuItem(10, "Exit Program", maxtextsize)
 	print()
 	
 	print()
@@ -422,9 +731,10 @@ local function DisplayAutomationParameterMenu()
 	
 	--write title
 	local padding = width / 2 + 14
-	gpu.setForeground(0xFF00FF)
+	gpu.setForeground(0xFF0000)
 	print(text.padLeft("AUTOMATIC CONTROL PARAMETERS", padding))
-	
+	print()
+
 	local labelsize=36
 	local valuesize=12
 	local maxtextsize=48
@@ -568,7 +878,7 @@ local function DisplayTestMenu()
 	padding = (width - (maxtextsize * 2)) / 3
 	gpu.setForeground(0xFF00FF)
 	io.write(text.padLeft("", padding))
-	io.write(text.padRight("Function Tests", maxtextsize+padding))
+	io.write(text.padRight("Function Tests", maxtextsize+padding+1))
 	io.write(text.padRight("Response Tests", maxtextsize))
 	print()
 
@@ -578,13 +888,10 @@ local function DisplayTestMenu()
 	print()
 	io.write(text.padLeft("", padding))
 	WriteMenuItem(2, "Alarm Functionality", maxtextsize+padding)
-	WriteMenuItem(4, "Battery High Power Response", maxtextsize)
-	print()
-	io.write(text.padLeft("", padding+1))
-	io.write(text.padRight("", maxtextsize+padding))
-	WriteMenuItem(5, "Battery Low Power Response", maxtextsize)
+	WriteMenuItem(4, "Battery High/Low Power Response", maxtextsize)
 	print()
 	gpu.setForeground(0xFFFFFF)
+	print("Unlisted number returns to Main Menu")
 	io.write("Selection: ")
 	local input = io.read()
 	return input
@@ -599,6 +906,12 @@ os.execute('clear')
 if response == "1" then
 print("Function Not Yet Implemented")
 elseif response == "2" then
+	StartReactor()
+elseif response == "3" then
+	StopReactor()
+elseif response == "4" then
+	DisplayData()
+elseif response == "5" then
 	subresponse = DisplayAutomationParameterMenu()
 	os.execute('clear')
 	if subresponse == "1" then
@@ -609,34 +922,28 @@ elseif response == "2" then
 		setMinBatteryPowerPercentage()
 	--4 is return to main menu doing nothing, so no extra code is necessary
 	end
-elseif response == "3" then
-	DisplayData()
-elseif response == "4" then
-print("Function Not Yet Implemented")
-elseif response == "5" then
-	StartReactor()
 elseif response == "6" then
-	StopReactor()
-elseif response == "7" then
-	SCRAMset()
-elseif response == "8" then
-	SCRAMreset()
-elseif response == "9" then
-print("Function Not Yet Implemented")
-elseif response == "10" then
 	subresponse = DisplayTestMenu()
 	os.execute('clear')
 	if subresponse == "1" then
 		SCRAMtest()
 	elseif subresponse == "2" then
 		Alarmtest()
+	elseif subresponse == "3" then
+		HeatHightest()
+	elseif subresponse == "4" then
+		Powertest()
 	end
-elseif response == "11" then
+elseif response == "7" then
+	SCRAMset()
+elseif response == "8" then
+	SCRAMreset()
+elseif response == "9" then
 	Alarmreset()
-elseif response == "12" then
+elseif response == "10" then
 	--don't do anything - including say it's a bad entry, 'cause it's not
 else
 	print("Invalid Selection: " .. response)
 end
-until response == "12"
+until response == "10"
 print("Program Terminating")
